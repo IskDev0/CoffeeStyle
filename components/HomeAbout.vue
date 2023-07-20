a1d05b237439e1de78504303fb2bd0466f82819f
Iskander <kubeev2001@gmail.com>
2023-07-20 18:39:47 +0600

    Added about page

--- components/HomeAbout.vue
+++ components/HomeAbout.vue
@@ -2,13 +2,9 @@
   <div class="home-about_container text-center flex flex-col items-center gap-4 pt-24">
 <h1 class="text-3xl">Even the all-powerful Pointing has no control about the blind texts.</h1>
   <p class="opacity-70">It is a paradisematic country, in which roasted parts of sentences fly into your mouth. Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life One day however a small line of blind text by the name of Lorem Ipsum decided to leave for the far World of Grammar.</p>
-  <NuxtLink class="text-[#A25F4B] home-about_link">Read the full Story</NuxtLink>
+  <NuxtLink to="/about" class="text-[#A25F4B] home-about_link">Read the full Story</NuxtLink>
   </div>
 </template>
-
-<script setup>
-
-</script>
 
 <style>
 
