9e6c1a54ca022c6673ce539004b1b9ae5a4118ad
Iskander <kubeev2001@gmail.com>
2023-07-31 12:47:22 +0600

    Deleted dashboard component

--- layouts/admin.vue
+++ layouts/admin.vue
@@ -3,7 +3,6 @@
     <aside class="w-1/6 h-screen bg-[#1D1F2E]">
       <ul class="flex flex-col text-white pt-10">
           <NuxtLink class="py-2 px-4 hover:bg-gray-700" to="/">Home</NuxtLink>
-          <NuxtLink class="py-2 px-4 hover:bg-gray-700" to="/admin/dashboard">Dashboard</NuxtLink>
           <NuxtLink class="py-2 px-4 hover:bg-gray-700" to="/admin/products">Products</NuxtLink>
           <NuxtLink class="py-2 px-4 hover:bg-gray-700" to="/admin/orders">Orders</NuxtLink>
         <NuxtLink class="py-2 px-4 hover:bg-gray-700" to="/admin/blogs">Blogs</NuxtLink>
--- pages/admin/dashboard.vue
+++
@@ -1,9 +0,0 @@
-<template>
-<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia, sit?</p>
-</template>
-
-<script setup lang="ts">
-definePageMeta({
-  layout: "admin"
-})
-</script>
